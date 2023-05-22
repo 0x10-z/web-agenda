c32090258d0f994f7a371db1c17e29440ce989d6
Iker Ocio <ikerocio@yahoo.es>
2023-05-22 14:19:24 +0200

    CORS updated

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,15 +2,12 @@ import os
 
 import uvicorn
 from fastapi import FastAPI, Request
-#from fastapi.middleware.cors import CORSMiddleware
-from starlette_csrf import CSRFMiddleware
-from starlette.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 from database import Base, SessionLocal, engine
 from models import create_initial_users
 from router import router
 from middleware import custom_csrf_middleware, db_session_middleware
-from fastapi.middleware.trustedhost import TrustedHostMiddleware
 
 app = FastAPI()
 
@@ -20,10 +17,13 @@ app.include_router(router)
 origins = os.getenv("ALLOWED_ORIGINS", "").split(",")
 print("ORIGINS: {}".format(origins))
 app.add_middleware(
-    TrustedHostMiddleware,
-    allowed_hosts=origins
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
-app.add_middleware(CSRFMiddleware, secret="__CHANGE_ME__")
+#app.add_middleware(CSRFMiddleware, secret="__CHANGE_ME__")
 #app.middleware("http")(custom_csrf_middleware)
 app.middleware("http")(db_session_middleware)
 
