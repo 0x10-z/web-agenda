c3500012a25710eefe94b8301f2255395befd0c3
Iker Ocio <iker@ikerocio.com>
2023-05-27 23:16:37 +0200

    Worker detected

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,19 +1,38 @@
-self.addEventListener("install", function (event) {
+const CACHE_VERSION = "v1.0";
+const CACHE_NAME = `my-app-${CACHE_VERSION}`;
+const CACHE_URLS = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/logo192.png",
+  "/favicon.ico",
+  "/sw.js",
+];
+
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open("mi-cache").then(function (cache) {
-      return cache.addAll([
-        "/index.html",
-        "/icon.png",
-        "/estilo.css",
-        "/app.js",
-      ]);
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CACHE_URLS))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName.startsWith("my-app-") && cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+          return null;
+        })
+      );
     })
   );
 });
 
-self.addEventListener("fetch", function (event) {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(function (response) {
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
   );
