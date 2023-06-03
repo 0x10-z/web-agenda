31ed0b1bf96faf11a9467e21c69132617c5aaf0d
iker ocio <iker@ikerocio.com>
2023-06-03 12:20:23 +0200

    Cypress fixed

--- frontend/e2e/cypress/support/commands.js
+++ frontend/e2e/cypress/support/commands.js
@@ -24,7 +24,7 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 Cypress.Commands.add("login", (username, password) => {
-  cy.visit("login");
+  cy.visit("/");
   cy.get("form").within(() => {
     cy.get('input[name="username"]').type(username);
     cy.get('input[name="password"]').type(password, { log: false });
